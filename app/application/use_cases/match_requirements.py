ee76bd12a0b33ebca9546f80f1c49beac840f00e
Kimblag <kimberly.blandong@gmail.com>
2026-02-01 17:59:23 -0300

    refactor(application): update match requirements use case

--- app/application/use_cases/match_requirements.py
+++ app/application/use_cases/match_requirements.py
@@ -1,20 +1,27 @@
-from app.application.exceptions.empty_requirement_file_exception import EmptyRequirementFileException
+from typing import Any
+
+from app.application.exceptions.empty_requirement_file_exception import (
+    EmptyRequirementFileException,
+)
 from app.application.ports.catalog_repository import CatalogRepository
 from app.application.ports.embedding_service import EmbeddingService
 from app.application.ports.file_reader import FileReader
-from app.application.ports.normalizer import NormalizerRequirement
+from app.application.ports.normalizer import Normalizer
 from app.application.ports.vector_repository import VectorRepository
 from app.domain.entities.catalog import Catalog
+from app.infrastructure.config import settings
 
 
 class MatchRequirements:
-    def __init__(self,
-                 file_reader: FileReader,
-                 normalizer: NormalizerRequirement,
-                 catalog_repository: CatalogRepository,
-                 embedding_service: EmbeddingService,
-                 vector_repository: VectorRepository,
-                 top_k: int = 3):
+    def __init__(
+        self,
+        file_reader: FileReader,
+        normalizer: Normalizer,
+        catalog_repository: CatalogRepository,
+        embedding_service: EmbeddingService,
+        vector_repository: VectorRepository,
+        top_k: int = 5,
+    ):
         self.file_reader = file_reader
         self.normalizer = normalizer
         self.catalog_repository = catalog_repository
@@ -22,63 +29,81 @@ class MatchRequirements:
         self.vector_repository = vector_repository
         self.top_k = top_k
 
-    def execute(self, file_path: str) -> list[dict]:
-        # read the requirement file
-        raw_items = self.file_reader.read_requirements(file_path)
-        
-        if len(raw_items) == 0:
-            raise EmptyRequirementFileException("Requirements does not contains any item.")
+    def execute(self, file_bytes: bytes) -> list[dict]:
+        raw_items = self.file_reader.read_requirements(file_bytes)
 
-        # normalize the requirement file
-        normalized_requirements = self.normalizer.normalize_requirements(raw_items)
-        
-        catalog: Catalog = self.catalog_repository.get()
-
-        # Create embeddings
-        matches_output = []
-
-        for requirement in normalized_requirements:
-            attributes_str = ",".join(f"{k}:{v}" for k, v in requirement.get('attributes', {}).items()) 
-            
-            text_to_embed = ( 
-                f"name: {requirement.get('name', '')} | " 
-                f"description: {requirement.get('description', '')} | " 
-                f"category: {requirement.get('category', '')} | " 
-                f"subcategory: {requirement.get('subcategory', '')} | " 
-                f"quantity: {requirement.get('quantity', '')} | " 
-                f"unit: {requirement.get('unit', '')} | " 
-                f"priority: {requirement.get('priority', '')} | " 
-                f"provider: {requirement.get('provider', '')} | " 
-                f"attributes: {attributes_str}" 
-                )
-            
-            embedding = self.embedding_service.get_embedding(text_to_embed)
-
-            top_matches = self.vector_repository.search(
-                query_embedding=embedding,
-                top_k=self.top_k
+        if not raw_items:
+            raise EmptyRequirementFileException(
+                "Requirements does not contain any item."
             )
 
-            enriched_matches = []
+        normalized_requirements = self.normalizer.normalize(raw_items=raw_items)
 
-            for item_id, score in top_matches:
+        catalog_items: list[dict[str, Any]] = self.catalog_repository.get()
+        catalog = Catalog()
+        catalog.batch_upsert(catalog_items)
+
+        output = self._get_matches(
+            normalized_requirements=normalized_requirements, catalog=catalog
+        )
+
+        return output
+
+    def _get_matches(self, normalized_requirements, catalog):
+        output = []
+
+        for requirement in normalized_requirements:
+            attributes_str = ",".join(
+                f"{k}:{v}" for k, v in requirement.get("attributes", {}).items()
+            )
+
+            text_to_embed = (
+                f"name: {requirement.get('name', '')} | "
+                f"description: {requirement.get('description', '')} | "
+                f"category: {requirement.get('category', '')} | "
+                f"subcategory: {requirement.get('subcategory', '')} | "
+                f"unit: {requirement.get('unit', '')} | "
+                f"provider: {requirement.get('provider', '')} | "
+                f"attributes: {attributes_str}"
+            )
+
+            embedding = self.embedding_service.get_embedding(text_to_embed)
+
+            candidates = self.vector_repository.search(
+                query_embedding=embedding,
+                top_k=self.top_k,
+            )
+
+            matches = []
+
+            for item_id, distance in candidates:
+                if distance > settings.MAX_DISTANCE:
+                    continue
+
                 item = catalog.get_item(item_id)
 
-                enriched_matches.append({
-                    "catalog_item_id": item.item_id,
-                    "name": item.name,
-                    "category": item.category,
-                    "subcategory": item.subcategory,
-                    "description": item.description,
-                    "unit": item.unit,
-                    "provider": item.provider,
-                    "attributes": item.attributes,
-                    "score": score
-                })
+                matches.append(
+                    {
+                        "catalog_item_id": item.item_id,
+                        "name": item.name,
+                        "category": item.category,
+                        "subcategory": item.subcategory,
+                        "description": item.description,
+                        "unit": item.unit,
+                        "provider": item.provider,
+                        "attributes": item.attributes,
+                        "score": distance,
+                    }
+                )
 
-            matches_output.append({
-                "requirement": requirement,
-                "matches": enriched_matches
-            })
+            # orden semántico explícito (menor distancia = mejor)
+            matches.sort(key=lambda m: m["score"])
 
-        return matches_output
+            output.append(
+                {
+                    "requirement": requirement,
+                    "matches": matches,
+                }
+            )
+
+        return output
