1c3ce939f57cada355fb4c75edb00f713fb4848a
Kimblag <kimberly.blandong@gmail.com>
2026-02-02 11:36:27 -0300

    refactor(infra): cleanup and organize infrastructure exceptions

---
+++ app/infrastructure/exceptions/__init__.py
@@ -0,0 +1,5 @@
+from app.infrastructure.exceptions.invalid_file_type_exception import InvalidFileTypeException
+
+__all__ = [
+    "InvalidFileTypeException",
+]
--- app/infrastructure/exceptions/infrastructure_exception.py
+++ app/infrastructure/exceptions/infrastructure_exception.py
@@ -1,3 +1,5 @@
 class InfrastructureException(Exception):
+    """Base exceptionfor infrastructure errors."""
+
     def __init__(self, message: str):
         super().__init__(message)
